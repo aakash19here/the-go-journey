cbf583f28198c76dd88a53b29a290006a4221b84
Aakash <aakash19here@gmail.com>
2026-03-01 14:07:06 +0530

    chore: inline structs

--- basics/structs/main.go
+++ basics/structs/main.go
@@ -15,6 +15,15 @@ func main() {
 		Email: "aakash19here@gmail.com",
 	}
 
+	c := struct {
+		Name string
+		Type string
+	}{
+		Name: "Toyota Camry",
+		Type: "Sedan",
+	}
+
 	fmt.Printf("The name is %s , age is %d and email is %s \n", p.Name, p.Age, p.Email)
+	fmt.Printf("The Car is %s , type is %s\n", c.Name, c.Type)
 
 }
