414e8e5b6ca159ed36d72b463d403dbfab835b5d
Aakash <aakash19here@gmail.com>
2026-03-02 23:54:20 +0530

    chore: add exercise

---
+++ basics/structs/exercise-2/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+)
+
+// 1. Define your struct here
+type Book struct {
+	ID         int
+	Title      string
+	Author     string
+	IsBorrowed bool
+}
+
+func main() {
+	// 2. Create your slice of books here
+	library := []Book{
+		{
+			ID:         1,
+			Title:      "Harry Potter",
+			Author:     "J.K Rowling",
+			IsBorrowed: false,
+		},
+		{
+			ID:         2,
+			Title:      "Do epic shit",
+			Author:     "Ankur Warikoo",
+			IsBorrowed: false,
+		},
+		{
+			ID:         3,
+			Title:      "48 Laws of Power",
+			Author:     "Robert Greene",
+			IsBorrowed: true,
+		},
+	}
+
+	ToggleStatus(library)
+
+	for _, value := range library {
+		if value.IsBorrowed {
+			fmt.Println("The Book is", value.Title, "by", value.Author, "is borrowed", value.IsBorrowed)
+		}
+	}
+
+}
+
+func ToggleStatus(lib []Book) []Book {
+	for i := range lib {
+		lib[i].IsBorrowed = !lib[i].IsBorrowed
+	}
+
+	return lib
+}
