dc1165f423eab11556602a475583f605f4549ffc
Aakash <aakash19here@gmail.com>
2026-03-18 14:50:51 +0530

    chore: multiple routines

--- intermediate/routines/channels/main.go
+++ intermediate/routines/channels/main.go
@@ -9,22 +9,20 @@ var wg = sync.WaitGroup{}
 
 func main() {
 	ch := make(chan int) // strongly typed channel
+	for range 5 {
+		wg.Add(2)
+		go func() {
+			i := <-ch
+			fmt.Println(i)
 
-	wg.Add(2)
-
-	go func() {
-		i := <-ch
-		fmt.Println(i)
-
-		wg.Done()
-	}()
-	go func() {
-		i := 42
-		ch <- i // pass the copy of i
-		i = 40  // receiving go routine won't care if we manipulate the value later
-		wg.Done()
-	}()
-
+			wg.Done()
+		}()
+		go func() {
+			i := 42
+			ch <- i // pass the copy of i
+			wg.Done()
+		}()
+	}
 	wg.Wait()
 
 }
