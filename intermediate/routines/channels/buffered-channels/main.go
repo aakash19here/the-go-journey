1bfafcbc75201cc12eb6c0b1fdf294940619a41e
Aakash <aakash19here@gmail.com>
2026-03-18 16:27:05 +0530

    chore: add buffered channels

---
+++ intermediate/routines/channels/buffered-channels/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var wg = sync.WaitGroup{}
+
+func main() {
+	// 50 is a buffer , create channel to create interal datastore to store 50 integers
+	ch := make(chan int, 50)
+
+	wg.Add(2)
+
+	// receive data from channel
+	go func(ch <-chan int) {
+		// for i := range ch {
+		// 	/*
+		// 		- Without a close(ch) ,  for loop introduces a deadlock, because now if the sender is done sending the data , we still keep on reading the values as the loop does not have any exit condition
+		// 	*/
+		// 	fmt.Println(i)
+		// }
+
+		// same like above , but more verbose
+		for {
+			if i, ok := <-ch; ok {
+				fmt.Println(i)
+			} else {
+				break
+			}
+		}
+
+		wg.Done()
+	}(ch)
+
+	// send data to chan
+	go func(ch chan<- int) {
+		ch <- 40
+		ch <- 42
+		ch <- 43
+		ch <- 44
+
+		/*
+			- Close means that the routine is done sending the values and by that the above receiver can know when to end the for loop
+			- When you close the channel make sure you don't send data after that otherwise the we'll get a panic
+		*/
+		close(ch)
+		wg.Done()
+	}(ch)
+
+	wg.Wait()
+}
